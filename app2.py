82e01af8b6bfb0014082d65879d455b47a8ad37a
Easymean1207 <dlwlasl1207@gmail.com>
2023-06-28 19:01:47 +0900

    sort student by name

--- app2.py
+++ app2.py
@@ -1,5 +1,8 @@
 students = [{'grade': '3', 'name':'lee jimin', 'dept':'CS' },
             {'grade': '2', 'name':'hong gildong', 'dept':'CE' },
-            {'grade': '3', 'name':'kim dongkwan', 'dept':'CS' }
+            {'grade': '3', 'name':'kim dongkwan', 'dept':'CS' },
             {'grade': '1', 'name':'Jetbrain', 'dept':'SE' },
             {'grade': '2', 'name':'Eclipse', 'dept':'Swift' }]
+
+students.sort(key=lambda x: x['name'])
+print(students)
