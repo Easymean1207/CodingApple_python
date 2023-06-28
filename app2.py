bdd4760c16398b29baeddf8c5c9bcdac33791fa7
Easymean1207 <dlwlasl1207@gmail.com>
2023-06-28 20:25:06 +0900

    동북아시아 국가 추가

--- app2.py
+++ app2.py
@@ -4,5 +4,8 @@ students = [{'grade': '3', 'name':'lee jimin', 'dept':'CS' },
             {'grade': '1', 'name':'Jetbrain', 'dept':'SE' },
             {'grade': '2', 'name':'Eclipse', 'dept':'Swift' }]
 
+country = ['japan','china','korea','taiwan','russia']
+
 students.sort(key=lambda x: x['name'])
-print(students)
+# print(students)
+print(country)
