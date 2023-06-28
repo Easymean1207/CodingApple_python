1ea6bcb0afb6818746786bcb46c4e59e1a5ac151
Easymean1207 <dlwlasl1207@gmail.com>
2023-06-28 18:50:58 +0900

    update students list

--- app2.py
+++ app2.py
@@ -1,3 +1,5 @@
 students = [{'grade': '3', 'name':'lee jimin', 'dept':'CS' },
             {'grade': '2', 'name':'hong gildong', 'dept':'CE' },
-            {'grade': '3', 'name':'kim dongkwan', 'dept':'CS' }]
+            {'grade': '3', 'name':'kim dongkwan', 'dept':'CS' }
+            {'grade': '1', 'name':'Jetbrain', 'dept':'SE' },
+            {'grade': '2', 'name':'Eclipse', 'dept':'Swift' }]
