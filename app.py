b0a94f1ea9aaf6aa7f2ef73b5c19f3e4dada0eb8
Easymean1207 <dlwlasl1207@gmail.com>
2023-06-30 16:12:43 +0900

    함수 이름 동사화

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import math
 
-#list
-car_info = ['k5','white',5000]
+# list
+car_info = ["k5", "white", 5000]
 
-#dictionary -> {key:value,key2:value2, ...}
-car_info2 = {
-    'brand':'bmw',
-    'model':'520d'
-}
+# dictionary -> {key:value,key2:value2, ...}
+car_info2 = {"brand": "bmw", "model": "520d"}
 
 inventory = 10
-car_inventory = ['k5','bmw','morning','spark','starrex','tico']
+car_inventory = ["k5", "bmw", "morning", "spark", "starrex", "tico"]
 
-#item in list
+# item in list
 """ if 'k5' in car_inventory :
     print('you can available right now')
 else :
@@ -21,27 +18,31 @@ else :
 """ for item in car_inventory:
     print(item * 3) """
 
-def perimeter(option: int) :
+
+def cal_perimeter(option: int):
     width = int(input("가로 값 : "))
     length = int(input("세로 값: "))
-    return (width + length) * 2 
+    return (width + length) * 2
 
-def width(option: int) :
+
+def cal_width(option: int):
     width = int(input("가로 값 : "))
     length = int(input("세로 값: "))
-    return (width * length) 
+    return width * length
 
-def choice(option: int) :
+
+def do_choice(option: int):
     result = 0
 
     if option == 1:
-        result = perimeter(option)
+        result = cal_perimeter(option)
         return result
     elif option == 2:
-        result = width(option)
+        result = cal_width(option)
         return result
-    else :
-        return -1;
+    else:
+        return -1
 
-option_val = choice(int(input("옵션을 선택하세요 1-> 둘레 2-> 넓이 : ")))
+
+option_val = do_choice(int(input("옵션을 선택하세요 1-> 둘레 2-> 넓이 : ")))
 print(option_val)
