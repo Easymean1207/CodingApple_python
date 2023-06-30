bd420c6a59f7f01084f34a0f8b394c43b44e3034
Easymean1207 <dlwlasl1207@gmail.com>
2023-06-30 15:16:50 +0900

    자동 저장 줄바꿈 반영

--- app2.py
+++ app2.py
@@ -1,11 +1,13 @@
-students = [{'grade': '3', 'name':'lee jimin', 'dept':'CS' },
-            {'grade': '2', 'name':'hong gildong', 'dept':'CE' },
-            {'grade': '3', 'name':'kim dongkwan', 'dept':'CS' },
-            {'grade': '1', 'name':'Jetbrain', 'dept':'SE' },
-            {'grade': '2', 'name':'Eclipse', 'dept':'Swift' }]
+students = [
+    {"grade": "3", "name": "lee jimin", "dept": "CS"},
+    {"grade": "2", "name": "hong gildong", "dept": "CE"},
+    {"grade": "3", "name": "kim dongkwan", "dept": "CS"},
+    {"grade": "1", "name": "Jetbrain", "dept": "SE"},
+    {"grade": "2", "name": "Eclipse", "dept": "Swift"},
+]
 
-country = ['japan','china','korea','taiwan','russia']
+country = ["japan", "china", "korea", "taiwan", "russia"]
 
-students.sort(key=lambda x: x['name'])
+students.sort(key=lambda x: x["name"])
 # print(students)
 print(country)
